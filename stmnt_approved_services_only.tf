1d4ba8806d19707362ee5ddae706af8bd65e0530
Michael Ullrich <michael.ullrich@acai.gmbh>
2025-03-19 22:15:22 +0100

    add comment

--- stmnt_approved_services_only.tf
+++ stmnt_approved_services_only.tf
@@ -1,4 +1,6 @@
 # Will restrict the AWS services to the listed ones for all prinvipals except the ones listed in the condition
+# Never combine two deny not_action statements with disjunct not_actions in single "OU-Hierarchy-Path"
+# Specify permitted services in a single statement / SCP. They cannot be accumulated across multiple statements.
 
 locals {
   approved_services_only = length(var.scp_settings.allowed_services) == 0 ? {} : {
